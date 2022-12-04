41253f81f39c79531242e20c801b8bb083992c36
ATP <atp@localhost>
2022-12-04 16:09:26 +0000

    part 1

---
+++ 04/part1.pl
@@ -0,0 +1,85 @@
+#!/usr/bin/perl
+
+$tot = 0;
+$i = 0;
+
+while(<>)
+{
+    
+ #1-2,2-96
+ if(/(\d+)\-(\d+),(\d+)\-(\d+)/)
+ {
+     $elf1a = $1;
+     $elf1b = $2;
+     $elf2a = $3;
+     $elf2b = $4;
+     
+     @elf1sectors = &getrange($elf1a, $elf1b);
+     @elf2sectors = &getrange($elf2a, $elf2b);
+
+
+     print @elf1sectors;
+     print "\n";
+     print @elf2sectors;
+     print "\n";
+     
+     $res1 = &checkrange(\@elf1sectors, \@elf2sectors);
+     print "RES1:: $res1 \n";
+     $res2 = &checkrange(\@elf2sectors, \@elf1sectors);
+
+     print "RES2:: $res2 \n";
+
+     print "\n=====\n";
+     
+     if ( $res1 || $res2)
+     {
+         $fullycontained++;
+     }
+
+ }
+ 
+ 
+}
+
+print "RESULT:: " . $fullycontained . "\n";
+
+sub getrange
+{
+    @r = ();
+    my ($a, $b) = @_;
+    
+    for ( $i = $a ; $i <= $b; $i++)
+    {
+        push(@r , $i);
+    }
+    return @r;
+}
+
+
+sub checkrange
+{
+    my $tot = 0;
+    my ($e1, $e2) = @_;
+    foreach $q (@$e1)
+    {
+        foreach $x (@$e2)
+        {
+            if ( $q eq $x )
+            {
+                $tot++;
+            }
+        }
+    }   
+    
+    $e1size = scalar @$e1;
+    print "\nTOT" . $tot . " SIZE :" . $e1size . "\n";
+    if ($e1size eq $tot)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
